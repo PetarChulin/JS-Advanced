d0fa228c1347b82d72d07a1d2ad4bf00d88fe3b8
PetarChulin <petercbg@mail.bg>
2022-05-20 22:28:50 +0300

    Update 5.SmallestTwoNumbers.js

--- Arrays and Nested Arrays - Lab/5.SmallestTwoNumbers.js
+++ Arrays and Nested Arrays - Lab/5.SmallestTwoNumbers.js
@@ -1,20 +1,7 @@
 function solve(array) {
 
-    let smallest = [];
-    let text = '';
-    for (let i = 0; i < 2; i++) {
-        
-        let min = Math.min(...array);
-        smallest.push(min);
-        text += String(min  + ' ');
-        array.splice(array.indexOf(min) , 1);             
-    }
-      
-console.log(text);
+    let smallestTwo = array.sort((a, b) => a - b).splice(0, 2).join(' ');
+
+    console.log(smallestTwo);
 }
 solve([3, 0, 10, 4, 7, 3]);
-
-// function sumTwoSmallestNumbers(numbers) {  
-//   numbers = numbers.sort((a, b) => {
-//     return a - b; });
-// }; 
