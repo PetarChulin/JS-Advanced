a0ea919ae3ac4525a3657d5338be6064aeaf9737
PetarChulin <petercbg@mail.bg>
2022-05-22 18:57:30 +0300

    Update 5.Assembly Line.js

--- Objects & Composition - Lab/5.Assembly Line.js
+++ Objects & Composition - Lab/5.Assembly Line.js
@@ -4,20 +4,20 @@ function createAssemblyLine() {
         hasClima: (car) => {
             car.temp = 21
             car.tempSettings = 21;
-            car.adjustTemp = () => {
+            car.adjustTemp = function adjust() {
                 car.temp < car.tempSettings ? car.temp++ : car.temp--;
             };
         },
         hasAudio: (car) => {
-            car[currentTrack] = { name: null, artist: null };
-            car.nowPlaying = () => {
+            car.currentTrack = { name: null, artist: null };
+            car.nowPlaying = function playing() {
                 car.currentTrack !== null ?
-                    console.log(`Now playing '${car[currentTrack][name]}' by ${car[currentTrack][artist]}`) : null;
+                    console.log(`Now playing '${car.currentTrack.name}' by ${car.currentTrack.artist}`) : null;
 
             };
         },
         hasParktronic: (car) => {
-            car.checkDistance = (distance) => {
+            car.checkDistance = function check(distance) {
                 distance < 0.1 ? console.log(`Beep! Beep! Beep!`) : null;
                 distance >= 0.1 && distance < 0.25 ? console.log('Beep! Beep!') : null;
                 distance >= 0.25 && distance < 0.5 ? console.log(`Beep!`) : null;
