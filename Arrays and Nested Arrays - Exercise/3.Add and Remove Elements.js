69ec3305699e1d2ff8acb9011bce97b0857330a4
PetarChulin <petercbg@mail.bg>
2022-05-23 18:24:36 +0300

    Update 3.Add and Remove Elements.js

--- Arrays and Nested Arrays - Exercise/3.Add and Remove Elements.js
+++ Arrays and Nested Arrays - Exercise/3.Add and Remove Elements.js
@@ -2,18 +2,13 @@ function addRemove(array) {
 
     let newArray = [];
     let counter = 0;
-    
+
     for (const element of array) {
         counter++;
-        if (element == 'add') {
-            newArray.push(counter);
-        } else if (element == 'remove') {
-            newArray.pop(counter);
-        }
+        element == 'add' ? newArray.push(counter) : null;
+        element == 'remove' ? newArray.pop(counter) : null;
     }
-    if (newArray.length == 0) {
-        console.log('Empty');
-    }
+    newArray.length == 0 ? console.log('Empty') : null;
     newArray.forEach(element => { console.log(element) });
 }
 addRemove(['add', 'add', 'add', 'add']);
