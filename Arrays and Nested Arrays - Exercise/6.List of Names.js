85315406ea2fb9695396e594bbe85787ed1608aa
PetarChulin <petercbg@mail.bg>
2022-05-23 18:41:17 +0300

    Update 6.List of Names.js

--- Arrays and Nested Arrays - Exercise/6.List of Names.js
+++ Arrays and Nested Arrays - Exercise/6.List of Names.js
@@ -1,7 +1,6 @@
 function sortNames(names) {
 
-    names.sort((a, b) => a.localeCompare(b));
+    names.sort((a, b) => a.localeCompare(b)).forEach((e, i) => console.log((i + 1) + '.' + e));
 
-    names.forEach((e , i) => console.log((i + 1) + '.' + e));
 }
 sortNames(["John", "Bob", "Christina", "Ema"]);
