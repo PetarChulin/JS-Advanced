88152a6fa95c55dbcdcdcba5a9b08a938bff0009
PeterC <petercbg@abv.bg>
2022-06-17 20:42:01 +0300

    Update bookSelection.test.js

--- JS Advanced - Exam/JS Advanced Retake Exam - 6 Apr 2022/3.book/bookSelection.test.js
+++ JS Advanced - Exam/JS Advanced Retake Exam - 6 Apr 2022/3.book/bookSelection.test.js
@@ -7,9 +7,8 @@ describe('Book Selection tests', () => {
     expect(bookSelection.isGenreSuitable('Thriller', 12)).to.equal(`Books with Thriller genre are not suitable for kids at 12 age`);
     expect(bookSelection.isGenreSuitable('Thriller', 10)).to.equal(`Books with Thriller genre are not suitable for kids at 10 age`);
     expect(bookSelection.isGenreSuitable('Horror', 12)).to.equal(`Books with Horror genre are not suitable for kids at 12 age`);
-    expect(bookSelection.isGenreSuitable('Horror', 10)).to.equal(`Books with Horror genre are not suitable for kids at 10 age`);
     expect(bookSelection.isGenreSuitable('Thriller', 14)).to.equal(`Those books are suitable`);
-    expect(bookSelection.isGenreSuitable('Horror', 15)).to.equal(`Those books are suitable`);
+    
   });
 
   it('test isItAffordable', () => {
